f14c69cb6551e942a6ab8cef91742621811b0865
Neo-Machina <andreistatache1997@gmail.com>
2022-07-21 17:25:24 +0200

    exercise completed

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<!-- Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, 
+dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,9 +13,34 @@
     <title>PHP-BADWORDS</title>
 </head>
 <body>
-    <?php 
 
+    <!-- Creazione variabili: stringa paragrafo e lunghezza-->
+    <?php 
+        $paragraph = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium nobis accusamus excepturi repellendus, perspiciatis quae dolorum dicta numquam ad quos totam repellat, repudiandae veniam. Veniam praesentium laudantium nesciunt provident amet?';
+        $paragraphLength = strlen($paragraph);
     ?>
+    
+    <!-- Stampo paragrafo e la sua lnghezza nel DOM -->
+    <p>TESTO ORIGINALE: <?php echo $paragraph ?> </p>
+
+    <div>LUNGHEZZA PARAGRAFO: <?php echo  $paragraphLength ?> </div>
+
+    <!-- Creazione variabili: parola passata dall'utente tramite parametro GET, stringa con parola censurata e lunghezza -->
+    <?php 
+        $word = $_GET['word'];
+
+        $replacedString = str_replace($word, '***', $paragraph);
+
+        $replacedStringLength = strlen($replacedString);
+    ?>
+
+    <!-- Stampo paragrafo e la sua lunghezza con parola censurate -->
+    <div>PAROLA DA CENSURARE: <?php echo $word ?></div>
+
+    <p>TESTO MODIFICATO: <?php echo $replacedString ?> </p>
+
+    <div>LUNGHEZZA PARAGRAFO MODIFICATO: <?php echo $replacedStringLength ?> </div>
+
 </body>
 </html>
 
